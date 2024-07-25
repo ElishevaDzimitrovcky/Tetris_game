944074a4c95442d6477900f5af709bd50672c48a
ElishevaDzimitrovcky <eli701128@gmail.com>
2024-07-25 15:23:28 +0300

    uploud game

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+
+import 'pages/intro_screen.dart';
+
+void main() async {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+        home: IntroScreen(),
+      );
+  }
+}
